521c2c4c5af5e0d2db5ee4b62ae32c603732e881
BlankZhu <BlankZhu47@gmail.com>
2022-02-14 12:49:38 +0800

    update: reset code structure

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,16 @@
-mod api;
 mod config;
-mod constant;
-mod log;
 mod option;
-mod pipeline;
-mod plugin;
-mod proxy;
-mod router;
 mod server;
-mod service;
 mod types;
 
-use std::io;
 use clap::Clap;
+use std::io;
 
 #[tokio::main]
 async fn main() -> io::Result<()> {
     // parse command line options
-    let opt = option::Options::parse();
-    
+    // ...
+
     // read config
     // ...
 
--- src/option/mod.rs
+++
@@ -1,9 +0,0 @@
-use clap::{AppSettings, Clap};
-
-#[derive(Clap)]
-#[clap(version = "1.0", author = "BlankZhu")]
-#[clap(setting = AppSettings::ColoredHelp)]
-pub struct Options {
-    #[clap(short, long, default_value = "config.yaml")]
-    pub config: String,
-}
