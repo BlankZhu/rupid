9d265e77d897b16378c28e7581e6471d435b552c
BlankZhu <BlankZhu47@gmail.com>
2022-02-18 16:21:32 +0800

    update: add timeout

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,9 @@ use warp::Filter;
 #[tokio::main]
 async fn main() {
     if std::env::var_os("RUST_LOG").is_none() {
-        std::env::set_var("RUST_LOG", "trace");
+        std::env::set_var("RUST_LOG", "debug");
     }
-    pretty_env_logger::init();
+    env_logger::init();
 
     let opt = Option::parse();
     let conf = match Config::load_from_yaml_file(&opt.config_filename) {
@@ -33,36 +33,47 @@ async fn main() {
     };
     info!("using config: {:?}", conf);
 
-    let routes = bing_search_api();
-    let svc = warp::service(routes);
-    let make_svc = warp::hyper::service::make_service_fn(move |_| async move {
-        Ok::<_, std::convert::Infallible>(svc)
+    // let routes = httpbin_org_api();
+    // let svc = warp::service(routes);
+    // let make_svc = warp::hyper::service::make_service_fn(move |_| async move {
+    //     Ok::<_, std::convert::Infallible>(svc)
+    // });
+    // let err = warp::hyper::Server::bind(&([127, 0, 0, 1], 3030).into())
+    //     .serve(make_svc)
+    //     .await;
+    // match err {
+    //     Ok(_) => return,
+    //     Err(e) => panic!("{}", e),
+    // }
+
+    let routes = httpbin_org_api();
+    let service = warp::service(routes);
+    let make_service = warp::hyper::service::make_service_fn(move |_| async move {
+        let service = tower::ServiceBuilder::new()
+            .timeout(std::time::Duration::from_millis(10))
+            .service(service);
+        Ok::<_, std::convert::Infallible>(service)
     });
-    let err = warp::hyper::Server::bind(&([127, 0, 0, 1], 3030).into())
-        .serve(make_svc)
-        .await;
-    match err {
-        Ok(_) => return,
-        Err(e) => panic!("{}", e),
-    }
+    let server = warp::hyper::Server::bind(&([127, 0, 0, 1], 7777).into()).serve(make_service);
+    server.await;
 }
 
-fn bing_search_api(
+fn httpbin_org_api(
 ) -> impl Filter<Extract = impl warp::Reply, Error = warp::Rejection> + Clone + Copy {
-    warp::get()
-        .and(warp::path::param::<String>())
+    warp::method()
+        .and(warp::filters::path::full())
         .and(warp::header::headers_cloned())
         .and(warp::body::stream())
-        .and_then(bing_search_callback)
+        .and_then(httpbin_org_callback)
 }
 
-async fn bing_search_callback(
-    search: String,
+async fn httpbin_org_callback(
+    method: http::Method,
+    path: warp::filters::path::FullPath,
     headers: http::HeaderMap,
     body: impl futures::Stream<Item = Result<impl bytes::buf::Buf, warp::Error>> + Send + 'static,
 ) -> Result<http::Response<warp::hyper::body::Body>, warp::Rejection> {
-    let request = bing_build_search_request(search, headers, body);
-    // todo: add tokio::time::timeout
+    let request = httpbin_org_build_request(method, format!("{}", path.as_str()), headers, body);
     let client = warp::hyper::Client::new();
 
     if let Ok(proxy_response) = client.request(request).await {
@@ -83,19 +94,20 @@ async fn bing_search_callback(
     }
 }
 
-fn bing_build_search_request(
-    search: String,
+fn httpbin_org_build_request(
+    method: http::Method,
+    path: String,
     headers: http::HeaderMap,
     body: impl futures::Stream<Item = Result<impl bytes::Buf, warp::Error>> + Send + 'static,
 ) -> http::Request<warp::hyper::Body> {
-    let uri = format!("http://cn.bing.com:80/search?q={}", search.as_str());
+    let uri = format!("http://httpbin.org:80/{}", path.as_str());
     let body = body.map_ok(|mut buf| buf.copy_to_bytes(buf.remaining()));
     let mut request = http::Request::new(warp::hyper::Body::wrap_stream(body));
-    *request.method_mut() = http::method::Method::GET;
+    *request.method_mut() = method;
     *request.uri_mut() = uri.parse().unwrap();
     *request.headers_mut() = headers;
     request
         .headers_mut()
-        .insert(http::header::HOST, "cn.bing.com".parse().unwrap());
+        .insert(http::header::HOST, "httpbin.org".parse().unwrap());
     request
 }
